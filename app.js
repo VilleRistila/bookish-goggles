9850b618192838838d9f729a5b5d37d6fc1052b1
Ville Ristilä <viljo_87@hotmail.com>
2024-11-23 15:53:10 +0200

    korjattu sqlinj

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts"; // For password has
 
 const app = new Hono();
 
+function validateUsername(username) {
+      const regex = /^[a-zA-Z0-9_]+$/;
+      return regex.test(username);
+}
+
 // Serve the registration form
 app.get('/register', async (c) => {
   return c.html(await Deno.readTextFile('./views/register.html'));
@@ -18,20 +23,22 @@ app.post('/register', async (c) => {
   const birthdate = body.birthdate;
   const role = body.role;
 
+  if (!validateUsername(username)) {
+    return c.text('Invalid username format.',400);
+  }
+  
   try {
     // Hash the user's password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     // Insert the new user into the database
+    const query = `INSERT INTO zephyr_users (username, password_hash, role, birthdate)
+                   VALUES ($1, $2, $3, $4)`;
     const result = await client.queryArray(
-      `INSERT INTO zephyr_users (username, password_hash, role, birthdate)
-       VALUES ($1, $2, $3, $4)`,
-      [username,
-      hashedPassword,
-      role,
-      birthdate]
-    );
+      //`INSERT INTO zephyr_users (username, password_hash, role, birthdate)
+      // VALUES ($1, $2, $3, $4)`,
+      query, [username, hashedPassword, role, birthdate]);
 
     // Success response
     return c.text('User registered successfully!');
